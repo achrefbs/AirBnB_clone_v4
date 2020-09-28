d73bc460bef8056ed8242b0fdeacefe7c50bed10
saidskander <askander199h@outlook.com>
2020-09-28 10:07:59 -0700

    Done

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,1 +1,23 @@
-n
+const $ = window.$;
+window.addEventListener('DOMContentLoaded', function (event) {
+  const dict = {};
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      dict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete dict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').html(Object.values(dict).join(', ') || '&nbsp;');
+  });
+});
+$.get('http://0.0.0.0:5001/api/v1/status/')
+  .done(function (response) {
+    if (response.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  })
+  .fail(function (response) {
+    $('#api_status').removeClass('available');
+  });
